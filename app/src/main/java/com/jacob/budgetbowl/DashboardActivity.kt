1e11da47d9e4dc469e50bab738d1215d7b65609f
Spiderbyte321 <143994901+Spiderbyte321@users.noreply.github.com>
2025-10-01 12:59:41 +0200

    Added a test fragment to the content drawer
    experimented with how to add to the content drawer

--- app/src/main/java/com/jacob/budgetbowl/DashboardActivity.kt
+++ app/src/main/java/com/jacob/budgetbowl/DashboardActivity.kt
@@ -34,6 +34,10 @@ class DashboardActivity : AppCompatActivity() {
                 .setAction("Action", null)
                 .setAnchorView(R.id.fab).show()
         }
+        //We do actually need the FAB so I'll set that up to add an expese
+        //uuuuuuuggghhh
+        //figure out how to add to content drawer then take a breather
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_dashboard)
@@ -41,9 +45,20 @@ class DashboardActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.tester
             ), drawerLayout
         )
+        //ok so I think we add to the content drawer witht eh appbarconfiguration
+        //then the rest will be handled by android
+        //almost there I think I just need to add it to the XML
+        //ok here's the order of operations
+        //Step 1 create the fragment XML in the layout folder
+        //Step 2 add that fragment to the mobile_navigation XML file in navigation
+        //either through code or the GUI
+        //Step 3 Set up the Item on the activity_main_drawer XML in the menu folder
+        //Step 4 add that fragment to the above appBarConfiguration
+        //N.B don't know if this works with activities or only fragments will test later
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
